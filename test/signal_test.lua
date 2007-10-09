49c9922cd9172b6ea7f8dbc6d17f622cf665b15a
jluehrs2 <jluehrs2@uiuc.edu>
2007-10-09 00:07:46 -0500

    fix the test script for the default name changes

--- test/signal_test.lua
+++ test/signal_test.lua
@@ -10,10 +10,10 @@ for i = 1, N do end
 signal.signal("INT", function() print("Got an interrupt!") end)
 print("Running now with a custom SIGINT handler...")
 for i = 1, N do end
-signal.signal("INT", "default")
+signal.signal("INT", "cdefault")
 print("Running now with the default SIGINT handler...")
 for i = 1, N do end
-signal.signal("INT", "luadefault")
+signal.signal("INT", "default")
 print("Running now with Lua's default SIGINT handler...")
 for i = 1, N do end
 print("Done!")
