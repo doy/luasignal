5e2c00529d52f90163695b88ac428f392f381fd4
jluehrs2 <jluehrs2@uiuc.edu>
2007-10-09 00:02:39 -0500

    testing script

---
+++ test/signal_test.lua
@@ -0,0 +1,19 @@
+require 'signal'
+
+N = 200000000
+
+print("Running before messing with SIGINT")
+for i = 1, N do end
+signal.signal("INT", "ignore")
+print("Running now with SIGINT ignored...")
+for i = 1, N do end
+signal.signal("INT", function() print("Got an interrupt!") end)
+print("Running now with a custom SIGINT handler...")
+for i = 1, N do end
+signal.signal("INT", "default")
+print("Running now with the default SIGINT handler...")
+for i = 1, N do end
+signal.signal("INT", "luadefault")
+print("Running now with Lua's default SIGINT handler...")
+for i = 1, N do end
+print("Done!")
